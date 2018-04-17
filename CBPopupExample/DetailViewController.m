9d981cb4ebb1152383d72de01ed8d1f16a9c6c38
changcun <qianchangcun@changba.com>
2018-04-17 11:15:59 +0800

    Modify Demo

--- CBPopupExample/DetailViewController.m
+++ CBPopupExample/DetailViewController.m
@@ -87,9 +87,7 @@
         vc.view.frame = CGRectMake(0, 0, self.view.bounds.size.width, 250);
     }
 
-//    [self cb_presentPopupViewController:vc animationType:[sender tag] aligment:self.popAligment overlayDismissed:nil];
-
-    [self cb_presentPopupViewController:vc animationType:[sender tag] aligment:CBPopupViewAligmentBottom margin:34 overlayDismissEnabled:YES overlayDismissed:nil];
+    [self cb_presentPopupViewController:vc animationType:[sender tag] aligment:self.popAligment overlayDismissed:nil];
 }
 
 @end
